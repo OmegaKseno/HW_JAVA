8b90e02357fe011fae7e594e3656590933bc8c17
Maksim.K <omega.kseno@gmail.com>
2023-04-21 00:43:50 +0300

    HW-3Task-1.

---
+++ src/main/java/org/example/Les3Tsk1/Goods.java
@@ -0,0 +1,17 @@
+package org.example.Les3Tsk1;
+
+public class Goods {
+    public String name;
+    public float price;
+    public Integer grade;
+
+
+    public Goods(String name, float price, Integer grade){
+        this.name = name;
+        this.price = price;
+        this.grade = grade;
+    }
+
+
+}
+
