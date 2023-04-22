e0064556396c0534241c0c848efc9613feae7b6f
Maksim.K <omega.kseno@gmail.com>
2023-04-22 23:33:29 +0300

    HW-3Task-3.

---
+++ src/main/java/org/example/Les3Tsk3/Books.java
@@ -0,0 +1,21 @@
+package org.example.Les3Tsk3;
+
+public class Books {
+    public String name;
+    public  String surname;
+    public float price;
+    public Integer year;
+
+    public Integer pages;
+
+
+    public Books(String name,String surname,float price ,  Integer year, Integer pages){
+        this.name = name;
+        this.surname = surname;
+        this.price = price;
+        this.year = year;
+        this.pages = pages;
+    }
+
+
+}
---
+++ src/main/java/org/example/Les3Tsk3/Task3.java
@@ -0,0 +1,46 @@
+package org.example.Les3Tsk3;
+import org.example.Les3Tsk3.Books;
+import java.util.ArrayList;
+import java.util.List;
+public class Task3 {
+    public static void main(String[] args) {
+        List<Books> product = getBooks();
+
+        for (Books prod : product) {
+            if(checkPrimeNumbers(prod.pages)&& prod.surname.startsWith("А")){
+                if(prod.year<=2010){
+                    System.out.println(prod.name);
+                }
+            }
+
+        }
+    }
+    public static ArrayList<Books> getBooks() {
+        ArrayList<Books> product = new ArrayList<Books>();
+        product.add(new Books("Книга1", "Антогов", 1.0f,2009, 79));
+        product.add(new Books("Книга2", "Пушкин", 2.0f,2006,89));
+        product.add(new Books("Книга3", "Андропов", 3.0f,2009,78));
+        product.add(new Books("Книга4", "Анисимов", 4.0f,2010,67));
+        product.add(new Books("Книга5", "Гоголь", 2.0f,1890,97));
+        return product;
+    }
+
+    /**
+     *
+     * @param num На вход целое число
+     * @return - Да если оно простое, Нет если не простое.
+     */
+    public static boolean checkPrimeNumbers (int num){
+        for (int i = 2; i < num; i++) {
+            if(num%i==0){
+                return false;
+            }
+        }
+        return true ;
+    }
+
+}
+
+
+
+
